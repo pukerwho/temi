ef79028c2e22dbe61100dd3a928da482e10f29e4
Puker Who <nawwesele@gmail.com>
2026-02-01 22:37:50 +0200

    edit

--- functions.php
+++ functions.php
@@ -134,6 +134,16 @@ function tarakan_scripts()
         true
     );
 
+    if (is_post_type_archive('articles')) {
+        wp_enqueue_script(
+            'articles-export',
+            get_template_directory_uri() . '/build/export-articles.js',
+            [],
+            null,
+            true
+        );
+    }
+
     if (is_singular() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
     }
